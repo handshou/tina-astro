16bc0f734117940d6edd3699e0f73db4ad4d1303
hansel <handshou@users.noreply.github.com>
2023-09-27 17:09:03 +0800

    add tina markdown to convert mdast

--- src/components/react.tsx
+++ src/components/react.tsx
@@ -1,11 +1,15 @@
 import { useTina } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
 
 export const MyComponent = (props) => {
-  const { data } = useTina(props);
+    const { data } = useTina(props);
 
-  return (
-    <pre style={{ margin: "100px auto", width: "1000px" }}>
-      {JSON.stringify(data?.post?.body, null, 2)}
-    </pre>
-  );
+    return (
+        <>
+            <TinaMarkdown content={data.post.body} />
+            <pre style={{ margin: "100px auto", width: "1000px" }}>
+                {JSON.stringify(data?.post?.body, null, 2)}
+            </pre>
+        </>
+    );
 };
